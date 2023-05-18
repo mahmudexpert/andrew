64f9b68b433934e90bba9175ff9552c9e417ab63
mahmudexpert <71225015+mahmudexpert@users.noreply.github.com>
2023-05-18 08:27:41 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -66,37 +66,3 @@
 
 })();
 
-// animated js
-new WOW().init();
-
-
-
-// Function to truncate the text and add "Read More" link
-function truncateText() {
-  const textElements = document.getElementsByClassName('text');
-
-  Array.from(textElements).forEach((element) => {
-    const content = element.innerHTML;
-    const maxLength = 100; // Maximum number of characters to show
-
-    if (content.length > maxLength) {
-      const truncatedContent = content.slice(0, maxLength) + '...';
-      const moreText = element.querySelector('.more-text');
-      const readMoreLink = element.nextElementSibling;
-
-      element.innerHTML = truncatedContent;
-      moreText.style.display = 'inline';
-      readMoreLink.style.display = 'inline';
-
-      readMoreLink.addEventListener('click', function (event) {
-        event.preventDefault();
-        element.innerHTML = content;
-        moreText.style.display = 'none';
-        readMoreLink.style.display = 'none';
-      });
-    }
-  });
-}
-
-// Call the function to truncate text when the page loads
-document.addEventListener('DOMContentLoaded', truncateText);
